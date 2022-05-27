ad52a7fbbda22fc3f5861b3cc619b90b021b509b
Lena Herrmann <herrmann-lena@t-online.de>
2022-05-27 14:10:58 +0200

    bars height depends now on every days amount, coming from data.json-file

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 fillTodaysBarCyan();
+fillBarsHeight();
 
 function fillTodaysBarCyan() {
     const todayAbbreviation = new Date().toLocaleDateString('en-US', { weekday: 'short' }).toLowerCase();
@@ -9,4 +10,19 @@ function fillTodaysBarCyan() {
             bar.childNodes[1].style.backgroundColor = 'var(--cyan)';
         }
     }); 
+}
+
+function fillBarsHeight() {
+    fetch('./data.json')
+        .then(res => res.json())
+        .then(data => {
+            const bars = document.querySelectorAll('.bar');
+            bars.forEach(bar => {
+                data.forEach(date => {
+                    if (bar.id === date.day) {
+                        bar.childNodes[1].style.height = `${date.amount}px`;
+                    }
+                }); 
+            }); 
+        });
 }
